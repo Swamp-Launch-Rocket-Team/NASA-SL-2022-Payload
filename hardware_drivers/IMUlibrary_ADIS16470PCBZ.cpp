bb1960ffe213a1ef9bf166001c5be099f5f35089
London Torres <londonztorres@gmail.com>
2022-02-25 00:17:37 -0500

    Add functions to read IMU

--- hardware_drivers/IMUlibrary_ADIS16470PCBZ.cpp
+++ hardware_drivers/IMUlibrary_ADIS16470PCBZ.cpp
@@ -1,79 +1,126 @@
-#include <iostream>
-#include <errno.h>
-#include <wiringPiSPI.h>
-#include <unistd.h>
+#include "IMUlibrary_ADIS16470PCBZ.h"
 
 using namespace std;
 
 #define CHANNEL 0
-#define BAUDRATE 1900000
-#define WRITE 1
-#define READ 0
+#define BAUDRATE 921600 // Max SCLK is 1 MHz when using burst reads
+// #define WRITE 1
+// #define READ 0
 
-void IMU_calibrate();
-unsigned short IMU_read_XXX();
-unsigned short SPI_RW_buffer_gen(unsigned char RW, unsigned char addr, unsigned char data);
-void IMU_setup();
+#define MSC_CTRL_ADDR 0x60
+#define GLOB_CMD_ADDR 0x68
 
-struct accel_t
+#define BURST_READ_DIN 0x6800
+#define BURST_READ_DIAG_STAT 1
+#define BURST_READ_X_GYRO_OUT 2
+#define BURST_READ_Y_GYRO_OUT 3
+#define BURST_READ_Z_GYRO_OUT 4
+#define BURST_READ_X_ACCL_OUT 5
+#define BURST_READ_Y_ACCL_OUT 6
+#define BURST_READ_Z_ACCL_OUT 7
+#define BURST_READ_TEMP_OUT 8
+#define BURST_READ_DATA_CNTR 9
+#define BURST_READ_CHECKSUM 10
+
+const float ACCEL_SCALER = 0.00122070312; // 40 / 2^15
+const float GYRO_SCALER = 0.0610351562; // 2000 / 2^15
+
+// static uint16_t SPI_RW_buffer_gen(uint8_t RW, uint8_t addr, uint8_t data)
+// {
+// 	unsigned short buffer;
+// 	buffer = RW << 15;
+// 	buffer |= addr << 8;
+// 	buffer |= data;
+// 	return buffer;
+// }
+
+static uint16_t IMU_SPI_read(uint8_t addr)
 {
-	float x, y, z;
-};
+	uint8_t buffer[4];
+	// buffer[0]: R/W, A6-A0
+	// buffer[1]: D7-D0, don't care on read
+	buffer[0] = ~(1 << 7) & addr;
+	wiringPiSPIDataRW(CHANNEL, buffer, 4);
+	return buffer[2] << 7 | buffer[3];
+}
 
-struct gyro_t
+static void IMU_SPI_write(uint8_t addr, uint8_t data)
 {
-	float x, y, z;
-};
-
-struct imu_data_t
-{
-	gyro_t gyro;
-	accel_t accel;
-};
+	uint8_t buffer[2];
+	// buffer[0]: R/W, A6-A0
+	// buffer[1]: D7-D0
+	buffer[0] = 1 << 7 | addr;
+	buffer[1] = data;
+	wiringPiSPIDataRW(CHANNEL, buffer, 2);
+}
 
 void IMU_setup()
 {
-int fd, result;
-   unsigned short buffer;
+	int fd, result;
+	unsigned short buffer;
+	cout << "Initializing" << endl ;
 
-   cout << "Initializing" << endl ;
+	// Configure the interface.
+	// CHANNEL insicates chip select,
+	// 500000 indicates bus speed.
+	fd = wiringPiSPISetup(CHANNEL, BAUDRATE);
 
-   // Configure the interface.
-   // CHANNEL insicates chip select,
-   // 500000 indicates bus speed.
-   fd = wiringPiSPISetup(CHANNEL, BAUDRATE);
-
-   cout << "Init result: " << fd << endl;
+	cout << "Init result: " << fd << endl;
 
 	/*Enabled linear g compensation, enabled point of percussion alignment,
-	Sync(CLK) set to default(internal clock), SYNC set to rising edge, DR polarity 
+	Sync(CLK) set to default(internal clock), SYNC set to rising edge, DR polarity
 	is set to active high*/
-	
-	buffer= SPI_RW_buffer_gen(WRITE,0x60,0xC3); //0xE0C3 
-	wiringPiSPIDataRW(CHANNEL, buffer, 2);
-	
+
+	// buffer = SPI_RW_buffer_gen(WRITE, MSC_CTRL_ADDR, 0xC3); //0xE0C3
+	// wiringPiSPIDataRW(CHANNEL, (unsigned char*)&buffer, 2);
+
+	IMU_SPI_write(MSC_CTRL_ADDR, 0xC3);
+
 	//Initial Bias Correction
 	IMU_calibrate();
-	
-}
-
-
-unsigned short SPI_RW_buffer_gen(unsigned char RW, unsigned char addr, unsigned char data)
-{
-
-	unsigned short buffer;
-	buffer = RW << 15;
-	buffer |= addr << 8;
-	buffer |= data;
-	return buffer;
 }
 
 void IMU_calibrate()
 {
-	unsigned short buffer;
-	buffer = SPI_RW_buffer_gen(WRITE, 0x68, 0x01); //Sets the bias correction value to true
-	wiringPiSPIDataRW(CHANNEL, buffer, 2);
+	// unsigned short buffer;
+	// buffer = SPI_RW_buffer_gen(WRITE, GLOB_CMD_ADDR, 0x01); //Sets the bias correction value to true
+	// wiringPiSPIDataRW(CHANNEL, (unsigned char*)&buffer, 2);
+
+	// Trigger bias correction using Continuous Bias Estimation (NULL_CNFG register)
+	IMU_SPI_write(GLOB_CMD_ADDR, 0x01);
 }
-unsigned short IMU_read_XXX(){
+
+imu_data_t IMU_read(){
+	imu_data_t imu_data;
+
+	// Burst Read
+	uint16_t buffer[11];
+	buffer[0] = BURST_READ_DIN;
+	memset(buffer+1, 0, sizeof(uint16_t)*10);
+	wiringPiSPIDataRW(CHANNEL, (uint8_t*)buffer, 22);
+
+	// TODO: Check DIAG_STAT and Checksum for error handling
+	uint16_t DIAG_STAT = buffer[BURST_READ_DIAG_STAT];
+	uint16_t TEMP_OUT = buffer[BURST_READ_TEMP_OUT];
+	uint16_t DATA_CNTR = buffer[BURST_READ_DATA_CNTR];
 	
+	// Data is signed and converted to a float-point value in real-world units
+	imu_data.accel.x = accel_format((int16_t)buffer[BURST_READ_X_ACCL_OUT]);
+	imu_data.accel.y = accel_format((int16_t)buffer[BURST_READ_Y_ACCL_OUT]);
+	imu_data.accel.z = accel_format((int16_t)buffer[BURST_READ_Z_ACCL_OUT]);
+	imu_data.gyro.x = gyro_format((int16_t)buffer[BURST_READ_X_GYRO_OUT]);
+	imu_data.gyro.y = gyro_format((int16_t)buffer[BURST_READ_Y_GYRO_OUT]);
+	imu_data.gyro.z = gyro_format((int16_t)buffer[BURST_READ_Z_GYRO_OUT]);
+
+	return imu_data;
+}
+
+static inline float accel_format(int16_t data)
+{
+	return data * ACCEL_SCALER;
+}
+
+static inline float gyro_format(int16_t data)
+{
+	return data * GYRO_SCALER;
 }
---
+++ hardware_drivers/IMUlibrary_ADIS16470PCBZ.h
@@ -0,0 +1,42 @@
+#ifndef IMU_H
+#define IMU_H
+
+#include <iostream>
+#include <errno.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <wiringPiSPI.h>
+#include <cstring>
+
+struct imu_data_t
+{
+	struct gyro_t
+	{
+		float x, y, z;
+	};
+	struct accel_t
+	{
+		float x, y, z;
+	};
+
+	gyro_t gyro;
+	accel_t accel;
+};
+
+// static uint16_t SPI_RW_buffer_gen(uint8_t RW, uint8_t addr, uint8_t data);
+
+void IMU_setup();
+
+void IMU_calibrate();
+
+imu_data_t IMU_read();
+
+static uint16_t IMU_SPI_read(uint8_t addr);
+
+static void IMU_SPI_write(uint8_t addr, uint8_t data);
+
+static inline float accel_format(int16_t data);
+
+static inline float gyro_format(int16_t data);
+
+#endif
